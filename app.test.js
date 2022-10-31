3454c2d4d98a83cb3804a95eea8edca390be136b
BhanukaAGH <it20009472@my.sliit.lk>
2022-10-31 16:02:18 +0530

    add another test cases

--- app.test.js
+++ app.test.js
@@ -44,6 +44,31 @@ describe('Test Ticketing System API', function () {
       })
       expect(response.body.token).toEqual(expect.any(String))
     })
+
+    it('POST /register again with same credentials', async function () {
+      const response = await request(app)
+        .post('/api/v1/auth/register')
+        .send({
+          name: 'Test User',
+          idNumber: 'testId123',
+          password: 'password',
+          role: 'local',
+        })
+        .set('Accept', 'application/json')
+
+      expect(response.status).toEqual(400)
+      expect(response.body.msg).toEqual('User already exists')
+    })
+
+    it('POST /login with invalid credentials', async function () {
+      const response = await request(app)
+        .post('/api/v1/auth/login')
+        .send({ idNumber: 'testId', password: 'wrong' })
+        .set('Accept', 'application/json')
+
+      expect(response.status).toEqual(401)
+      expect(response.body.msg).toEqual('Invalid Credentials')
+    })
   })
 
   describe('User API', function () {
@@ -62,6 +87,43 @@ describe('Test Ticketing System API', function () {
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
         __v: expect.any(Number),
+      })
+    })
+
+    it('GET / user with invalid id', async function () {
+      const id = '123'
+      const response = await request(app)
+        .get(`/api/v1/user/${id}`)
+        .set('Accept', 'application/json')
+        .set('authorization', userToken)
+
+      expect(response.status).toEqual(404)
+      expect(response.body.msg).toEqual(`No item found with id : ${id}`)
+    })
+
+    it('GET / user with invalid token', async function () {
+      const response = await request(app)
+        .get(`/api/v1/user/${userID}`)
+        .set('Accept', 'application/json')
+        .set('authorization', 'invalid token')
+
+      expect(response.status).toEqual(401)
+      expect(response.body.msg).toEqual(`Authentication Invalid`)
+    })
+
+    it('PUT / update password with invalid credentials', async function () {
+      const response = await request(app)
+        .put(`/api/v1/user/update-password`)
+        .send({
+          oldPassword: 'invalid',
+          newPassword: 'secret',
+        })
+        .set('Accept', 'application/json')
+        .set('authorization', userToken)
+
+      expect(response.status).toEqual(401)
+      expect(response.body).toEqual({
+        msg: 'Invalid Credentials',
       })
     })
 
