13a96c26f1cbc01d5ec50192ed5230ea5c8611d8
HasithKovinda <it20032906@my.sliit.lk>
2022-11-02 20:46:42 +0530

    bug fixed

--- routes/creditRoutes.js
+++ routes/creditRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const {
   addCredit,
   getCreditBalance,
+  deleteCredit
 } = require('../controllers/creditController')
 
 router.route('/').post(addCredit)
 router.route('/:userId').get(getCreditBalance)
+router.route("/:id").delete(deleteCredit);
 
 module.exports = router
