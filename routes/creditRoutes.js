74765aa074faf834a98deff6482f11f2c97c1ecd
Buddheesha Induwara <it20031602@my.sliit.lk>
2022-11-08 06:23:09 +0530

    get balance credit controller added and bugs fixes

--- routes/creditRoutes.js
+++ routes/creditRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const {
   addCredit,
   getCreditBalance,
-  deleteCredit
+  deleteCredit,
+  getCreditBalanceByIDNo,
 } = require('../controllers/creditController')
 
 router.route('/').post(addCredit)
 router.route('/:userId').get(getCreditBalance)
-router.route("/:id").delete(deleteCredit);
+router.route('/:id').delete(deleteCredit)
+router.route('/getBal/:idNumber').get(getCreditBalanceByIDNo)
 
 module.exports = router
